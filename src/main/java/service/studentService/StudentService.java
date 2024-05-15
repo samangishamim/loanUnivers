65959269ebbdd4adb29270f19f4e0d45cfab347c
shamim <shamimsamangi5@gmail.com>
2024-05-15 14:15:48 +0330

    create the service and serviceImpl for the student clas

--- src/main/java/service/studentService/StudentService.java
+++ src/main/java/service/studentService/StudentService.java
@@ -1,4 +1,12 @@
 package service.studentService;
 
-public interface StudentService {
+import base.service.BaseService;
+import model.Student;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface StudentService extends BaseService<Student, Long> {
+    public List<Student> studentSignIn(String nationalId, String password);
+    public Optional<Student> studentInfo(String nationalId);
 }
--- src/main/java/service/studentService/StudentServiceImpl.java
+++ src/main/java/service/studentService/StudentServiceImpl.java
@@ -1,4 +1,46 @@
 package service.studentService;
 
-public class StudentServiceImpl {
+import base.service.BaseServiceImpl;
+import connection.SessionFactorySingleton;
+import exeption.NotFoundException;
+import model.Student;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import repository.studentRepository.StudentRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class StudentServiceImpl extends BaseServiceImpl<Student, Long , StudentRepository>
+        implements StudentService{
+    private final SessionFactory sessionFactory = SessionFactorySingleton.getInstance();
+    public StudentServiceImpl(StudentRepository repository, SessionFactory sessionFactory) {
+        super(repository, sessionFactory);
+    }
+
+    @Override
+    public List<Student> studentSignIn(String nationalId, String password) {
+        try (Session session = sessionFactory.getCurrentSession()) {
+            session.beginTransaction();
+            Optional<List<Student>> find = repository.studentSignIn(nationalId, password);
+            find.orElseThrow(() -> new NotFoundException("Entity not found"));
+            session.getTransaction().commit();
+            return find.get();
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    @Override
+    public Optional<Student> studentInfo(String nationalId) {
+        try (Session session = sessionFactory.getCurrentSession()) {
+            session.beginTransaction();
+            Optional<Student> find = repository.studentInfo(nationalId);
+            find.orElseThrow(() -> new NotFoundException("Entity not found"));
+            session.getTransaction().commit();
+            return find;
+        } catch (Exception e) {
+            return Optional.empty();
+        }
+    }
 }
